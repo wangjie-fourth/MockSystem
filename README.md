454128e569bf0927cceb246cbe6324a0a1f3d6df
wangjie <jwang55@trip.com>
2023-05-15 16:57:21 +0800

    feat: 新增attach包

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 java -javaagent:./agent/target/agent-1.0.jar -jar ./test-application/target/test-application-1.0-jar-with-dependencies.jar
 java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5055 -javaagent:./agent/target/agent-1.0.jar -jar ./test-application/target/test-application-1.0-jar-with-dependencies.jar
 ```
+
+未完事项：
+1、动态attach需要给一个命令行执行的例子，现在缺少tool.jar
--- test-application/src/main/java/org/github/fourth/mocksystem/testapplication/MyAtm.java
+++ test-application/src/main/java/org/github/fourth/mocksystem/testapplication/MyAtm.java
@@ -6,13 +6,18 @@ package org.github.fourth.mocksystem.testapplication;
 public class MyAtm {
 
     public static void withdrawMoney(int amount) throws InterruptedException {
-        Thread.sleep(1000);
+        Thread.sleep(5000);
+        System.out.println("start==============================");
         System.out.printf("[Application] Successful Withdrawal of [{%s}] units!%n", amount);
     }
 
     public static void main(String[] args) {
         try {
-            withdrawMoney(5);
+            while (true) {
+                withdrawMoney(5);
+                System.out.println("end==============================");
+                System.out.println("\n");
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
